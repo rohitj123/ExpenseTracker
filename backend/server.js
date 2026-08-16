6bde4a28d857e6bafde85b99103794bba3864f12
Rohit Jain <145150180+rohitj123@users.noreply.github.com>
2026-08-17 01:35:25 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -28,7 +28,7 @@ const app = express();
 const port = process.env.PORT || 4000;
 
 // Middlewares
-app.use(cors({ origin: 'https://vercel.app' }));
+app.use(cors());
 app.use(express.json({ limit: "5mb" }));
 app.use(express.urlencoded({ extended: true, limit: "5mb" }));
 
